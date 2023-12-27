4c32bbea6c9b0a6c5247fee445ff9625a7703037
Oleg <naitmaneev@gmail.com>
2023-12-27 10:50:42 +0300

    finished taskEvenOdd

--- src/main/java/org/example/TaskEvenOdd.java
+++ src/main/java/org/example/TaskEvenOdd.java
@@ -1,2 +1,16 @@
-package org.example;public class TaskEvenOdd {
+package org.example;
+
+import java.util.Scanner;
+
+public class TaskEvenOdd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        if (number % 2 == 0){
+            System.out.println("Чётное!");
+        } else {
+            System.out.println("Не чётное!");
+        }
+        scan.close();
+    }
 }
