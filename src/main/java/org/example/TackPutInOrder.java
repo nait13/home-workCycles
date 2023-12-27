ae1a5af45d88c96e86459eaed110697bdad49c54
Oleg <naitmaneev@gmail.com>
2023-12-27 11:22:38 +0300

    added TaskPutInOrder

---
+++ src/main/java/org/example/TackPutInOrder.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class TackPutInOrder {
+    public static void main(String[] args) {
+        int number = 7;
+        while (number <= 98) {
+            System.out.print(number + " ");
+            number+=7;
+        }
+    }
+}
