19b78a71b8923e2b22bf3d50161e62c8aba333e4
Oleg <naitmaneev@gmail.com>
2023-12-27 11:15:19 +0300

    added TaskSquaring

---
+++ src/main/java/org/example/TaskSquaring.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class TaskSquaring {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 20; i++) {
+            int c = i * i;
+            System.out.println("Квадрат " + i + " :" + c );
+        }
+    }
+}
